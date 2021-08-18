b97315bf8ede9abd8a22338717466d9dacd2c87e
Mouna1001 <66981481+Mouna1001@users.noreply.github.com>
2021-08-18 19:32:09 +0100

    update

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ const app = express();
 app.use(express.static('./dist/dev-demo'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/dev-demo/'}),
+    res.sendFile('index.html', { root: 'dist/dev-demo/' }),
 );
+app.use(cors());
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
